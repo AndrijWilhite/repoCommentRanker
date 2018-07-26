8d064271ee21b3d872252b05cb7fdf0922cfdff1
Andrij <andrij.wilhite@gmail.com>
2018-07-25 22:32:54 -0700

    implemented rate limit to progress bar and a check to handle rate limit issue

--- index.js
+++ index.js
@@ -10,26 +10,34 @@ const _cliProgress = require('cli-progress');
 
 var repo;
 var period;
+const comments = [];
+const userStat=[];
+const users =[];
+const final = [];
+
+
 
 //gets param for repo
 if(process.argv.indexOf("--repo") != -1){
     repo = process.argv[process.argv.indexOf("--repo") + 1];
-}
+};
 //gets param for period and removes non digit character
 if(process.argv.indexOf("--period") != -1){
     period = process.argv[process.argv.indexOf("--period") + 1].replace(/\D/g,'');
-}
+};
 
 //simple check to ensure both params required are passed
 if(period == undefined || repo == undefined){
     let msg = "Please ensure that you have passed the 'repo' flag and the 'period' flag, as well as pass valid params for them."
     console.log(msg);
     process.exit(1);
-}
+};
 
 console.log('Fetching comments for past ' + period + ' days for "' + repo + '"...');
-const bar1 = new _cliProgress.Bar({fps: 60,}, _cliProgress.Presets.shades_classic);
-bar1.start(100, 0)
+
+//builds the progress bar and starts it
+const progressBar = new _cliProgress.Bar({fps: 60, format: 'progress [{bar}] {percentage}% | RateLimit: {rateLimit}/5000'}, _cliProgress.Presets.shades_classic);
+// progressBar.start(100, 0)
 
 const apiBase = 'https://api.github.com';
 const http = axios.create({
@@ -39,28 +47,27 @@ const http = axios.create({
     },
   });
 
-var comments = [];
-
 
 const setDay = (moment().dayOfYear()) - period;
+//converts the date into day of year and compares against the day of year for the comment
 function dateCheck(comment){
     let postDate = moment(comment.created_at).dayOfYear();
     if(postDate >= setDay){
         comments.push(comment);
-    }
-}
+    };
+};
 
 //cycles through the various comment end points and hits them, pushes to Comments
 async function getComments() {
     var reqEnd = ['/comments', '/issues/comments', '/pulls/comments']
     for(const ending of reqEnd){
         if(ending == '/comments'){
-        bar1.update(20);
+        progressBar.update(20);
         }else if(ending == '/issues/comments'){
-            bar1.update(40);
+            progressBar.update(40);
         }else if(ending == '/issues/comments'){
-            bar1.update(60);
-        }
+            progressBar.update(60);
+        };
 
         try {
             const response = await http.get('/repos/' + repo + ending)
@@ -70,14 +77,13 @@ async function getComments() {
         } catch (err) {
             console.error(chalk.red(err))
             console.dir(err.response.data, { colors: true, depth: 4 })
-        }
+        };
     };
-}
+};
 
-var userStat=[]
-
+//hits the stats/commit api
 async function getStats() {
-    bar1.update(80);
+    progressBar.update(80);
         try {
             const response = await http.get('/repos/' + repo + '/stats/contributors')
             response.data.forEach(stat => {
@@ -86,58 +92,132 @@ async function getStats() {
         } catch (err) {
             console.error(chalk.red(err))
             console.dir(err.response.data, { colors: true, depth: 4 })
+        };
+};
+
+async function getRateLimit() {
+        try {
+            const response = await http.get('/rate_limit')
+            return response.data.rate;
+
+        } catch (err) {
+            console.error(chalk.red(err))
+            console.dir(err.response.data, { colors: true, depth: 4 })
+        };
+};
+
+// function run(){
+//     getRateLimit().then(function(rate){
+//         if(rate <= 4){
+
+//         }
+//         progressBar.start(100, 0, {
+//             rateLimit: rate
+//         })
+//     });
+//     //hits comments apu
+//     getComments().then(function(){
+//         for(const comment of comments){
+//             users.push(comment.user.login);
+//         };
+//         let userComCount = _.countBy(users);
+//         //hits stats api
+//         getStats().then(function(){
+//             //thats commit data and the comments data and and pushes into their own data set
+//             userStat.forEach(function(person){
+//                 let keys = Object.keys(userComCount);
+//                 keys.forEach(function(key){
+//                     if(person.name == key){
+//                         final.push({'name': person.name, 'comments':userComCount[key], 'commits': person.commits})
+//                     };
+//                 });
+//             });
+
+//             //reverse so that highest comment is in position 0
+//             let sortedFinal = _.sortBy(final, 'comments').reverse();
+//             //maxLength is used for leftPad 
+//             var maxLength = "";
+//             progressBar.update(100);
+
+//             //prints the people and their comment/commit info from the final array
+//             sortedFinal.forEach(function(person){
+//                 if (maxLength == ""){
+//                     //required to handle spacing issue with progress bar
+//                     console.log(" ");
+//                     maxLength += (person.comments+"").length
+//                     let result = person.comments+" comments, "+person.name+" ("+person.commits+" commits)";
+//                     console.log(result);
+//                 }else {
+//                     let comLength = maxLength - (person.comments+"").length
+//                     let result = leftPad(person.comments, comLength+1)+" comments, "+person.name+" ("+person.commits+" commits)";
+//                     console.log(result);
+//                 };
+//             });
+//             progressBar.stop();
+//         })
+//     });
+// };
+
+
+function checkRate(){
+    getRateLimit().then(function(rate){
+        if(rate.remaining >= 4){
+            progressBar.start(100, 0, {
+                rateLimit: rate.remaining
+            })
+            runPoll();
+        }else if(rate.remaining <= 3){
+            var waitTime = moment.unix(rate.reset).toString()
+            console.log('You have reach the Rate Limit for this key, please wait until'+ waitTime);        
         }
-    
-}
+    });
+};
 
-const users =[];
-
-const final = []
-
-function run(){
+function runPoll(){
     getComments().then(function(){
         for(const comment of comments){
             users.push(comment.user.login);
         };
         let userComCount = _.countBy(users);
-
+        //hits stats api
         getStats().then(function(){
+            //thats commit data and the comments data and and pushes into their own data set
             userStat.forEach(function(person){
                 let keys = Object.keys(userComCount);
                 keys.forEach(function(key){
                     if(person.name == key){
                         final.push({'name': person.name, 'comments':userComCount[key], 'commits': person.commits})
-                    }
-                })
+                    };
+                });
+            });
 
-            })
+            //reverse so that highest comment is in position 0
             let sortedFinal = _.sortBy(final, 'comments').reverse();
+            //maxLength is used for leftPad 
             var maxLength = "";
-            bar1.update(100);
+            progressBar.update(100);
 
+            //prints the people and their comment/commit info from the final array
             sortedFinal.forEach(function(person){
                 if (maxLength == ""){
+                    //required to handle spacing issue with progress bar
                     console.log(" ");
                     maxLength += (person.comments+"").length
                     let result = person.comments+" comments, "+person.name+" ("+person.commits+" commits)";
                     console.log(result);
-
                 }else {
                     let comLength = maxLength - (person.comments+"").length
                     let result = leftPad(person.comments, comLength+1)+" comments, "+person.name+" ("+person.commits+" commits)";
                     console.log(result);
-
-                }
-
-
+                };
             });
-            bar1.stop();
+            progressBar.stop();
         })
     });
-    
 }
 
-run();
+
+checkRate();
 
 
 
